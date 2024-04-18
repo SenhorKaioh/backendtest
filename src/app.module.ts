ba5f33a17e630f0ba9582678309054a6f89969ec
jorgeguedess <jorge_guedes00@hotmail.com>
2024-04-18 14:45:23 -0300

    feat: adicionado a entidade Categoria

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Categoria } from './categoria/entities/categoria.entity';
 
+// Mudar configuracao
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root@123',
+      database: 'db_greenconomy',
+      entities: [Categoria],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/categoria/entities/categoria.entity.ts
@@ -0,0 +1,20 @@
+import { IsNotEmpty } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'tb_categoria' })
+export class Categoria {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @IsNotEmpty()
+  @Column({ length: 255, nullable: false })
+  tipo: string;
+
+  @IsNotEmpty()
+  @Column({ length: 255, nullable: false })
+  slug: string;
+
+  @IsNotEmpty()
+  @Column({ length: 255, nullable: false })
+  foto: string;
+}
