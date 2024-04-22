67102ac26268e56b570411efec1dca4bc2f0c51e
kotecbr@outlook.com <kotecbr@outlook.com>
2024-04-22 11:36:06 -0300

    adicionado metodo findAll

--- src/categoria/controllers/categoria.controller.ts
+++ src/categoria/controllers/categoria.controller.ts
@@ -1,1 +1,16 @@
 // TODO - IMPLEMENTAR CATEGORIA CONTROLLER
+
+import { Controller, Get, HttpCode, HttpStatus } from "@nestjs/common";
+import { CategoriaService } from "../services/categoria.service";
+import { Categoria } from "../entities/categoria.entity";
+
+@Controller("/categorias")
+export class CategoriaController{
+    constructor(private readonly categoriaService: CategoriaService){}
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    findAll(): Promise<Categoria[]>{
+        return this.categoriaService.findAll();
+    }
+}
