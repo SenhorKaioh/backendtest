fc9819410ac982446c098cbf70f1e63d3867ec60
jorgeguedess <jorge_guedes00@hotmail.com>
2024-04-22 10:59:06 -0300

    chore: adicionado variaveis de ambientes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Categoria } from './categoria/entities/categoria.entity';
+import { ConfigModule } from '@nestjs/config';
 
-// Mudar configuracao
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env', '.env.development'],
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
       database: 'db_greenconomy',
       entities: [Categoria],
+      synchronize: true,
     }),
   ],
   controllers: [AppController],
