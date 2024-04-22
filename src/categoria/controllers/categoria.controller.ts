d0c04d07ce437ae1afdf9627b91f583b51a5cc77
StephanieSST <stephaniesteuernagel@gmail.com>
2024-04-22 14:52:08 -0300

    feat: Método Uptade

--- src/categoria/controllers/categoria.controller.ts
+++ src/categoria/controllers/categoria.controller.ts
@@ -1,31 +1,47 @@
 // TODO - IMPLEMENTAR CATEGORIA CONTROLLER
 
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post } from "@nestjs/common";
-import { CategoriaService } from "../services/categoria.service";
-import { Categoria } from "../entities/categoria.entity";
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CategoriaService } from '../services/categoria.service';
+import { Categoria } from '../entities/categoria.entity';
 
-@Controller("/categorias")
-export class CategoriaController{
-    constructor(private readonly categoriaService: CategoriaService){}
+@Controller('/categorias')
+export class CategoriaController {
+  constructor(private readonly categoriaService: CategoriaService) {}
 
-    @Get()
-    @HttpCode(HttpStatus.OK)
-    findAll(): Promise<Categoria[]>{
-        return this.categoriaService.findAll();
-    }
-    @Get("/:id")
-    @HttpCode(HttpStatus.OK)
-    findById(@Param("id", ParseIntPipe) id: number): Promise<Categoria>{
-        return this.categoriaService.findById(id);
-    }
-    @Get('/tipo/:tipo')
-    @HttpCode(HttpStatus.OK)
-    findByTipo(@Param('tipo') tipo: string): Promise<Categoria[]>{
-        return this.categoriaService.findByTipo(tipo);
-    }
-    @Post()
-    @HttpCode(HttpStatus.CREATED)
-    create(@Body() categoria: Categoria): Promise<Categoria>{
-        return this.categoriaService.create(categoria);
-    }
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll(): Promise<Categoria[]> {
+    return this.categoriaService.findAll();
+  }
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Categoria> {
+    return this.categoriaService.findById(id);
+  }
+  @Get('/tipo/:tipo')
+  @HttpCode(HttpStatus.OK)
+  findByTipo(@Param('tipo') tipo: string): Promise<Categoria[]> {
+    return this.categoriaService.findByTipo(tipo);
+  }
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() categoria: Categoria): Promise<Categoria> {
+    return this.categoriaService.create(categoria);
+  }
+
+  @Put()
+  @HttpCode(HttpStatus.OK)
+  update(@Body() categoria: Categoria): Promise<Categoria> {
+    return this.categoriaService.update(categoria);
+  }
 }
--- src/categoria/services/categoria.service.ts
+++ src/categoria/services/categoria.service.ts
@@ -1,46 +1,55 @@
-// TODO - IMPLEMENTAR CATEGORIA SERVICE
-
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Categoria } from "../entities/categoria.entity";
-import { ILike, Repository } from "typeorm";
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Categoria } from '../entities/categoria.entity';
+import { ILike, Repository } from 'typeorm';
 
 @Injectable()
-export class CategoriaService{
-    constructor(
-        @InjectRepository(Categoria)
-        private categoriaRepository: Repository<Categoria>
-    ){}
+export class CategoriaService {
+  constructor(
+    @InjectRepository(Categoria)
+    private categoriaRepository: Repository<Categoria>,
+  ) {}
 
-    async findAll(): Promise<Categoria[]>{
-        return await this.categoriaRepository.find();
-    }
-
-    async findById(id: number): Promise<Categoria>{
-        
-        const categoria = await this.categoriaRepository.findOne({
-            where: {
-              id,
-            },
-          });
-      
-          if (!categoria)
-            throw new HttpException(
-              'Categoria não encontrada!',
-              HttpStatus.NOT_FOUND,
-            );
-      
-          return categoria;
-        }
-
-    async findByTipo(tipo:string): Promise<Categoria[]>{
-      return await this.categoriaRepository.find({
-        where:{
-          tipo: ILike(`%${tipo}%`)
-        }
-      })
+  async findAll(): Promise<Categoria[]> {
+    return await this.categoriaRepository.find();
   }
-  async create(categoria: Categoria): Promise<Categoria>{
+
+  async findById(id: number): Promise<Categoria> {
+    const categoria = await this.categoriaRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!categoria)
+      throw new HttpException(
+        'Categoria não encontrada!',
+        HttpStatus.NOT_FOUND,
+      );
+
+    return categoria;
+  }
+
+  async findByTipo(tipo: string): Promise<Categoria[]> {
+    return await this.categoriaRepository.find({
+      where: {
+        tipo: ILike(`%${tipo}%`),
+      },
+    });
+  }
+  async create(categoria: Categoria): Promise<Categoria> {
     return await this.categoriaRepository.save(categoria);
-    }
-    }
+  }
+
+  async update(categoria: Categoria): Promise<Categoria> {
+    const buscaCategoria = await this.findById(categoria.id);
+
+    if (!buscaCategoria)
+      throw new HttpException(
+        'Categoria não encontrada!',
+        HttpStatus.NOT_FOUND,
+      );
+
+    return await this.categoriaRepository.save(categoria);
+  }
+}
