8a2a34fa589f53bcef6852ceb34c2845799023c3
Caio <caiopsantos.dev@gmail.com>
2024-04-26 11:21:11 -0300

    feat: Adicionando a controller do Auth e Jwt

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,18 @@
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { jwtConstants } from '../constants/constants';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      JwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: jwtConstants.secret,
+    });
+  }
+  async validate(payload: any) {
+    return { userId: payload.sub, username: payload.username };
+  }
+}
