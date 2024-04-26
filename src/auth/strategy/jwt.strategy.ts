4dd8644cf6bde3ac450e91ef08b8e26610a903ca
jorgeguedess <jorge_guedes00@hotmail.com>
2024-04-26 13:12:32 -0300

    fix: corrigido o erro de JwtStrategy

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,18 +1,19 @@
-import { Strategy, ExtractJwt } from 'passport-jwt';
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 import { jwtConstants } from '../constants/constants';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      JwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: jwtConstants.secret,
     });
   }
+
   async validate(payload: any) {
-    return { userId: payload.sub, username: payload.username };
+    return { sub: payload.sub };
   }
 }
